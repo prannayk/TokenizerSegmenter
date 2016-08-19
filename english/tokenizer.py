0a5cee3157f067c74ea113e37a61832f34e24a3e
Prannay Khosla <prannay.khosla@gmail.com>
2016-08-19 20:08:48 +0530

    Working tokenizer

--- english/tokenizer.py
+++ english/tokenizer.py
@@ -6,7 +6,7 @@ print("Using the treebank lexicon")
 words = treebank.words()
 tagged_words = treebank.tagged_words()
 tokenizer = HfstTokenizer()
-skip_symbols = [',','.',' ','!','?','#']
+skip_symbols = [',','.',' ','!','?','#','-']
 
 for word in skip_symbols:
 	tokenizer.add_skip_symbol(word)
@@ -16,7 +16,13 @@ for word in words:
 
 
 # stemming for tokenization
-test = "This some randomized test data which is not so random. I'm okay with it! Aren't you? The question is, what does the fox say, or rather the foxes say?"
+test = "shan't can't wouldn't This some randomized test data which is not so random. I'm okay with it! Aren't you? The question is, what does the fox say, or rather the foxes say? Corp. Mr. Dr. www.prannayk.com google.com"
 
-stringlist = tokenizer.tokenize_one_level(test)
-print(stringlist)
+stringlist = test.split(" ")
+tokens = []
+for string in stringlist:
+	temptoken =  tokenizer.tokenize_one_level(string)
+	if len(temptoken) > 1:
+		tokens += string
+	else:
+		tokens += temptoken
