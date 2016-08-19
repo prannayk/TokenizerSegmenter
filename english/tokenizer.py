ca8f6217a56a3e244b239a1bcc572cf756590bae
Prannay Khosla <prannay.khosla@gmail.com>
2016-08-19 22:44:03 +0530

    working

--- english/tokenizer.py
+++ english/tokenizer.py
@@ -9,8 +9,8 @@ words += brown.words()
 tagged_words = treebank.tagged_words()
 tagged_words += brown.words()
 tokenizer = HfstTokenizer()
-skip_symbols = [',','.',' ','!','?','#','-']
-
+kip_symbols = [',','.',' ','!','?','#','-']
+skip_symbols = []
 analyser = HfstBasicTransducer()
 
 for word in skip_symbols:
@@ -20,8 +20,18 @@ for word in words:
 		tokenizer.add_multichar_symbol(word)
 print("Preprocessing completed")
 
-test = "shan't can't wouldn't This some randomized test data which is not so random. I'm okay with it! Aren't you? The question is, what does the fox say, or rather the foxes say? Corp. Mr. Dr. www.prannayk.com google.com"
+test = "shan't can't wouldn't This some randomized test data which is not so random. I'm okay with it! Aren't you? The question is, what does the fox say, or rather the foxes say? Corp. Mr. Dr. www.prannayk.com google.com players cricketers balls"
 
+def ignore(tokens):
+	i = 0
+	for symbols in kip_symbols:
+		if tokens.count(symbols):
+			i+=tokens.count(symbols)
+	if i is 1:
+		return True
+	return False
+
+#rint(tokenize(test))
 def analyze(token):
 	return pos_tag(token)
 
@@ -31,11 +41,19 @@ def tokenize(text):
 	for word in text:
 		temptokens = tokenizer.tokenize_one_level(word)
 		if len(temptokens) > 1:
-			tokens += (word,)
+			if not ignore(word):
+				tokens += (word,)
+			else:
+				tokens += temptokens
 		else:
 			tokens += temptokens
 	return tokens
 
+tokenized = tokenize(test)
+
 analysed = {}
-for tokens in tokenize(test):
+for tokens in tokenized:
 	analysed.update(analyze((tokens,)))
+
+for token in tokenized:
+	print(token + ":" + analysed[token])
